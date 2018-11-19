32b23650918cd5c76668f4e96a7e277f0b2a81d0
Minzhi Liao <liao02x@gmail.com>
2018-11-18 17:35:56 -0800

    add hw07

--- bcl/app.R
+++ bcl/app.R
@@ -5,54 +5,112 @@ library(dplyr)
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
 ui <- fluidPage(
-  titlePanel("BC Liquor Store prices"),
+  includeCSS("www/bcl.css"),
+  tags$div(class = "title", titlePanel("BC Liquor Store prices")),
   sidebarLayout(
     sidebarPanel(
       sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
-      radioButtons("typeInput", "Product type",
-                  choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
-                  selected = "WINE"),
-      uiOutput("countryOutput")
+      sliderInput("binsNumber", "Bins number", 5, 95, 30, step = 5),
+      checkboxGroupInput("typeInput", "Product type",
+                   choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
+                   selected = "WINE"),
+      uiOutput("countryOutput"),
+      actionButton("selectAll", "Select all countries", value = FALSE),
+      br(), br(),
+      hr(),
+      span("Data source:", 
+           tags$a("OpenDataBC",
+                  href = "https://www.opendatabc.ca/dataset/bc-liquor-store-product-price-list-current-prices")),
+      br(),
+      span("Built following this tutorial: ", a(href = "http://deanattali.com/blog/building-shiny-apps-tutorial/", "Building shiny apps tutorial")),
+      br(), br(),
+      em(
+        span("Created by Minzhi Liao"),
+        HTML("&bull;"),
+        span("Code", a(href = "https://github.com/STAT545-UBC-students/hw08-liao02x", "on GitHub"))
+      )
     ),
     mainPanel(
-      plotOutput("coolplot"),
-      br(), br(),
-      tableOutput("results")
+      tags$h3(tags$em(textOutput("textresult"))),
+      hr(),
+      tabsetPanel(
+        tabPanel("Plot", plotOutput("coolplot")),
+        tabPanel("Table", DT::dataTableOutput("results"))
+      ),
+      hr(),
+      downloadButton("downloadData", "Download result data"),
+      downloadButton("downloadPlot", "Download result plot")
     )
   )
 )
 
 server <- function(input, output) {
-  output$countryOutput <- renderUI({
-    selectInput("countryInput", "Country",
-                sort(unique(bcl$Country)),
-                selected = "CANADA")
-  })  
-  
   filtered <- reactive({
     if (is.null(input$countryInput)) {
       return(NULL)
     }    
-    
     bcl %>%
       filter(Price >= input$priceInput[1],
              Price <= input$priceInput[2],
-             Type == input$typeInput,
-             Country == input$countryInput
-      )
+             Type %in% input$typeInput,
+             Country %in% input$countryInput)
+  })
+  myplot <- reactive({
+    ggplot(filtered(), aes(Alcohol_Content, fill = Type)) +
+      geom_histogram(bins = input$binsNumber)
+  })
+  output$countryOutput <- renderUI({
+    if (input$selectAll) {
+      selectInput("countryInput", "Country",
+                  sort(unique(bcl$Country)),
+                  selected = unique(bcl$Country), multiple = TRUE)
+    }
+    else {
+      selectInput("countryInput", "Country",
+                  sort(unique(bcl$Country)),
+                  selected = "CANADA", multiple = TRUE)
+    }
   })
   
   output$coolplot <- renderPlot({
     if (is.null(filtered())) {
       return()
     }
-    ggplot(filtered(), aes(Alcohol_Content)) +
-      geom_histogram()
+    myplot()
   })
-
-  output$results <- renderTable({
+  
+  output$results <- DT::renderDataTable({
     filtered()
   })
+  output$textresult <- renderText({
+    paste("We found ", nrow(filtered()), " option(s) for you.")
+  })
+  output$downloadData <- downloadHandler(
+    filename = function () {
+      paste0("data-", 
+             input$priceInput[0],
+             "-",
+             input$priceInput[1],
+             ".csv")
+    },
+    content = function(file) {
+      write.csv(filtered(), file)
+    },
+    contentType = "text/csv"
+  )
+  output$downloadPlot <- downloadHandler(
+    filename = function () {
+      paste0("data-", 
+             input$priceInput[0],
+             "-",
+             input$priceInput[1],
+             ".png")
+    },
+    content = function(file) {
+      ggsave(file, myplot())
+    },
+    contentType = "image/png"
+  )
 }
 
 shinyApp(ui = ui, server = server)
